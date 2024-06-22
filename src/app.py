b1dfc72d680db59087aa9ddc287a4906055729ff
jordidel170 <jordidel170@gmail.com>
2024-06-22 18:04:52 +0100

    Commit Jordi 22-06

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from Crypto.Cipher import AES
+
 import base64
 import json
 
